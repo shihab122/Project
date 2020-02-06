151f471cdc64d64965fe61b5b533a619d2f73ccb
shihab <shihabhossain611@gmail.com>
2020-02-06 18:00:46 +0600

    shihab122

--- Node.Js/devcamper_api/server.js
+++ Node.Js/devcamper_api/server.js
@@ -1,20 +1,44 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// const logger = require('./middleware/logger.js');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+// Load env
+dotenv.config({ path: './config/config.env' });
+
+// Connect DB
+connectDB();
 
 // Route Files
 const bootcamps = require('./routes/bootcamps');
 
-// Load env
-dotenv.config({ path: './config/config.env' });
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Middleware
+// app.use(logger);
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `App run in ${process.env.NODE_ENV} mode and listening on port ${process.env.PORT}!`
+      .yellow.bold
   )
 );
+
+// Handled unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
